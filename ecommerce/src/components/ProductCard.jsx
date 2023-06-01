fc7d5d85fabb74142e3eb4b24cdeddb3330116ac
kareem alaa <kareemalaanaga98@gmail.com>
2023-06-01 23:06:52 +0300

    out of stock quantity and disabled add to cart button and view icon

--- ecommerce/src/components/ProductCard.jsx
+++ ecommerce/src/components/ProductCard.jsx
@@ -16,21 +16,21 @@ export function ProductCard(props) {
           <Card.Title className='my-3'>{product.name}</Card.Title>
 
           <Card.Text>
-            Price : {product.price}
+            Price : {'$' + product.price}
           </Card.Text>
           <Card.Text className='text-dark'>
-            <p>
+            <p className={product.quantity === 0 ? "text-danger" : product.quantity === 1 ? "text-primary" : ""} >
               {product.quantity === 1 ? 'Only One Item Left' : product.quantity === 0 ? 'Out Of Stock' : `Quantity available: ${product.quantity}`}
             </p>
           </Card.Text>
-          <NavLink to={`/products/${product.id}`}>
-            <i className="mx-5 fs-2 bi bi-eye-fill ms-1">
-            </i>
-          </NavLink>
-          <button className='cardbtn btn-outline' disabled={product.quantity == 0}>
+          {product.quantity === 0 ? (<i className="mx-5 fs-2 bi bi-eye-fill ms-1 disabled cardicon" />)
+            : (<NavLink to={`/products/${product.id}`}>
+              <i className="mx-5 fs-2 bi bi-eye-fill ms-1" />
+            </NavLink>
+            )}
+          <button className='cardbtn' disabled={product.quantity == 0}>
             {product.quantity == 0 ? 'Out Of Stock' : 'Add To Cart'}
           </button>
-
         </Card.Body>
       </Card>
     </div>
